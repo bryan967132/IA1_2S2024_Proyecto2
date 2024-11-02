b67c502845f63c9fa1ef5b24e64033999b210b4c
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2024-11-01 19:49:50 -0600

    [UPDATE] CSV Parser

--- js/script.js
+++ js/script.js
@@ -9,12 +9,25 @@ const updateModelName = () => {
 
 var contentFile = null
 
+const parseCSV = (data) => {
+    const rows = data.trim().split("\n"); // Separar por líneas
+    const headers = rows[0].split(","); // Suponiendo que la primera fila contiene los encabezados
+    const results = rows.slice(1).map(row => {
+        const values = row.split(",");
+        return headers.reduce((obj, header, i) => {
+            obj[header.trim()] = values[i].trim();
+            return obj;
+        }, {});
+    });
+    return results;
+}
+
 const readFileContent = (event) => {
     const file = event.target.files[0];
     if (file && file.type === "text/csv") {
         const reader = new FileReader();
         reader.onload = (e) => {
-            contentFile = e.target.result;
+            contentFile = parseCSV(e.target.result);
             document.getElementById('actions').innerHTML = `<h3>Actions</h3><button type="button" onclick="train()" class="btn btn-primary">Train</button>`
         };
         reader.readAsText(file, "UTF-8");
@@ -42,7 +55,7 @@ const models = {
 const train = () => {
     const modelSelect = getSelectedOption()
     models[modelSelect].reset()
-    models[modelSelect].train()
+    models[modelSelect].train(contentFile)
     if(models[modelSelect].isTrained) {
         document.getElementById('actions').innerHTML += `<button type="button" onclick="predict()" class="btn btn-primary">Predict</button>`
     }
